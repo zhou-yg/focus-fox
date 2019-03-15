cb6033685ff845ee651d47db792da61f56e4fbe0
zhouyg <haraldred@163.com>
2019-03-15 17:39:39 +0800

    add center

---
+++ wan-front2/src/components/main/game/GameCenter.tsx
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from "react";
+
+
+interface GameCenterProps {
+    data: WanCategoryPushed;
+    onGameStart: () => void;
+}
+
+
+function GameCenter(props: GameCenterProps) {
+
+  return (<div className="main-game-center">
+  
+  </div>);
+}
+
+export default GameCenter;
